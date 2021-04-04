bb7e9d8e91ecd4b6e90c22c2187d1fd382ec3e9a
samin891 <69497674+samin891@users.noreply.github.com>
2021-04-04 10:54:52 +0900

    OK

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -31,8 +31,8 @@ class CarInterface(CarInterfaceBase):
 
     ret.carName = "hyundai"
     ret.safetyModel = car.CarParams.SafetyModel.hyundaiLegacy
-    #if candidate in [CAR.SONATA]:
-    #  ret.safetyModel = car.CarParams.SafetyModel.hyundai
+    if candidate in [CAR.SONATA]:
+      ret.safetyModel = car.CarParams.SafetyModel.hyundai
 
 
     params = Params()
